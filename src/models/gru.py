16a99a038318a092c9a91f7f1d71cf82cbf7b16b
Sebastian Aas <sebastian.aas@outlook.com>
2020-10-27 14:16:44 +0100

    bug/load-data

--- src/models/gru.py
+++ src/models/gru.py
@@ -2,14 +2,14 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Dropout, TimeDistributed, Dense, Activation, GRU, Input
 
 def gru_model(hidden_layers, shape, optimizer, loss, use_dropout=False):
-    inputs = x = Input(shape=(shape.shape[1], shape.shape[2]))
+    inputs = Input(shape=(shape.shape[1], shape.shape[2]))
     x = GRU(hidden_layers[0], return_sequences=False)(inputs)
     for i in range(1,len(hidden_layers)):
         x = GRU(hidden_layers[i])(x)
     if use_dropout:
-        inputs = Dropout(0.5)(x)
+        x = Dropout(0.2)(x)
     outputs = Dense(1)(x)
     model = Model(inputs, outputs)
-    model.compile(optimizer=optimizer, loss=loss)
+    model.compile(optimizer=optimizer, loss=loss, metrics=["mae"])
     return model
 
