55a9a81d82142bd2c7f6a7c2ac35c1992118b45b
Mattis <mattislt@sf-nett.no>
2020-11-10 12:49:01 +0100

    feat: added logging of experiment results

--- src/train.py
+++ src/train.py
@@ -14,11 +14,12 @@ from models.simple_rnn import simple_rnn_model
 from utils.csv_utils import read_csv
 from utils.load_data import load_data
 from utils.plotting import plot_predictions
+import datetime
 
 
 def train_model(save_dir, name, model, epochs, dataset_train, dataset_val):
     # Train
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="src/logs")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='src/logs')
     history = model.fit(
         dataset_train,
         epochs=epochs,
@@ -26,7 +27,7 @@ def train_model(save_dir, name, model, epochs, dataset_train, dataset_val):
         callbacks=[tensorboard_callback],
     )
     Path(save_dir).mkdir(parents=True, exist_ok=True)
-    model.save("{}/{}.h5".format(save_dir, name))
+    model.save('{}/{}.h5'.format(save_dir, name))
     return model, history
 
 
@@ -35,8 +36,8 @@ def run_experiments():
         for seq_length in config.sequence_lengths:
             for name in config.models:
                 run_experiment(model_name=name,
-                               hidden_layers=config.models.get(name)["hidden_layers"],
-                               learning_rate=config.models.get(name)["learning_rate"],
+                               hidden_layers=config.models.get(name)['hidden_layers'],
+                               learning_rate=config.models.get(name)['learning_rate'],
                                sequence_length=seq_length,
                                features=config.experiment_features.get(j)
                                )
@@ -48,17 +49,17 @@ def run_experiment(model_name,
                    sequence_length,
                    features
                    ):
-    flattend = True if model_name == "fully_connected" else False
+    flattend = True if model_name == 'fully_connected' else False
     train_data, train_dataset, val_data, val_dataset, test_data, test_dataset = load_data(features, flattend, sequence_length)
 
-    save_dir = "experiments/{}-sequence-length-{}-features-{}".format(model_name, sequence_length, len(features))
+    save_dir = 'experiments/{}-sequence-length-{}-features-{}'.format(model_name, sequence_length, len(features))
     Path(save_dir).mkdir(parents=True, exist_ok=True)
 
     for batch in train_data.take(1):
         inputs, targets = batch
         break
 
-    if model_name == "fully_connected":
+    if model_name == 'fully_connected':
         model = fully_connected_model(
             hidden_layers=hidden_layers,
             learning_rate=learning_rate,
@@ -66,7 +67,7 @@ def run_experiment(model_name,
             loss=config.loss,
             use_dropout=True
         )
-    elif model_name == "lstm":
+    elif model_name == 'lstm':
         model = lstm_model(
             hidden_layers=hidden_layers,
             learning_rate=learning_rate,
@@ -74,7 +75,7 @@ def run_experiment(model_name,
             loss=config.loss,
             use_dropout=True
         )
-    elif model_name == "simple_rnn":
+    elif model_name == 'simple_rnn':
         model = simple_rnn_model(
             hidden_layers=hidden_layers,
             learning_rate=learning_rate,
@@ -100,10 +101,12 @@ def run_experiment(model_name,
         dataset_train=train_data,
         dataset_val=val_data
     )
-    print("Evaluate on test data")
+    print('Evaluate on test data')
     results = trained_model.evaluate(test_data, batch_size=32)
-    print("test loss, test acc:", results)
-    visualize_loss(history, "Training and Validation loss", save_dir)
+    print('test loss, test acc:', results)
+    with open('experiments/experiment_results.txt', 'a') as myfile:
+        myfile.write('{}, {}-sequence-length-{}-features-{}, {}, {}\n'.format(datetime.datetime.now(), model_name, sequence_length, len(features), results[0], results[1]))
+    visualize_loss(history, 'Training and Validation loss', save_dir)
 
     if config.plot_all_predictions:
         plot_predictions(test_data, test_dataset, trained_model, sequence_length, save_dir, True)
